3b948f77d2c2acc8e20c07f619d6ae4f709b443f
WGXWGX <wgx15145076862@sina.com>
2016-11-25 10:51:12 +0800

    Create demo8.js

---
+++ demo8.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div id="app"></div>
+	<script src="react-0.14.7/build/react.js"></script>
+	<script src="react-0.14.7/build/react-dom.js"></script>
+	<script src="react-0.14.7/browser.js"></script>
+
+</body>
+</html>
