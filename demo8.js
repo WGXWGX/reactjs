658f41dabe276e5fceab347b0ecaede3801ce025
WGXWGX <wgx15145076862@sina.com>
2016-11-25 10:51:32 +0800

    Update demo8.js

--- demo8.js
+++ demo8.js
@@ -9,6 +9,21 @@
 	<script src="react-0.14.7/build/react.js"></script>
 	<script src="react-0.14.7/build/react-dom.js"></script>
 	<script src="react-0.14.7/browser.js"></script>
-
+		<script type="text/babel">
+		var App = React.createClass({
+			clickHandler : function(e){
+				console.log(this.refs.btn);
+			},
+			render : function(){
+				return (
+					<button onClick={this.clickHandler} ref='btn'>点我</button>
+				);
+			}
+		});
+		ReactDOM.render(
+			<App/>,
+			document.getElementById('app')
+		);
+	</script>
 </body>
 </html>
